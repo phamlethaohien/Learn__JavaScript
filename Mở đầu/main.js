142eca08e35eca6284b5ab93f1258d5720cd37e7
phamlethaohien <thaohienvn2007@gmail.com>
2021-07-01 11:13:10 +0700

    Comment

---
+++ Mở đầu/main.js
@@ -0,0 +1,18 @@
+//  Comment : Ctrl + /
+
+// Khai báo biến
+var fullName = 'Pham Le Thao Hien';
+var age = '14';
+
+// Notification
+/* alert(age);
+alert(fullName);
+*/
+
+/**
+ * abc
+ * bcd
+ * cde
+ * def
+ */
+
