fd87ea92ff3de75f31f72bb440d97d92adf2e553
phamlethaohien <thaohienvn2007@gmail.com>
2021-07-02 15:51:17 +0700

    Arithmetic

---
+++ Toán tử/Arithmetic/main.js
@@ -0,0 +1,31 @@
+// ARITHMETIC
+let a = 1;
+let b = 2;
+let c = a + b
+
+console.log(c);
+
+var x = 9;
+x++;
+console.log(x);
+
+// Prefix & Postfix
+var y = 6;
+var v = ++y;
+
+console.log('v:', v);
+
+var z = 5;
+var u = z++;
+
+console.log('u:', u);
+console.log(z);
+
+var number = 6;
+var output = ++number - number--;
+// 7 - 7 = 0
+
+console.log('output:', output);
+
+
+
