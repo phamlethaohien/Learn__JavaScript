456fcea58e9c87484967ebf54fd981c6b6004e9d
phamlethaohien <thaohienvn2007@gmail.com>
2021-07-03 08:45:54 +0700

    string operator

---
+++ Toán tử/String/main.js
@@ -0,0 +1,14 @@
+// STRING
+var firstName = 'Crystal';
+var lastName = 'Kei';
+
+console.log(firstName + ' ' + lastName);
+
+var name = 'Crystal';
+name += ' Kei';
+
+console.log(name);
+<<<<<<< 518558a54160a6ef1724e98de50af32dbc638dba
+
+=======
+>>>>>>> String
