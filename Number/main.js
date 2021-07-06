15489ce3370fffea96dc37defad6d1e5b323f6ee
phamlethaohien <thaohienvn2007@gmail.com>
2021-07-06 10:27:27 +0700

    So va lam viec voi so

---
+++ Number/main.js
@@ -0,0 +1,15 @@
+var age = 14;
+var PI = 3.14;
+var amount = 1000.0809473841;
+
+// To String
+console.log(age.toString());
+
+// To Fixed
+console.log(PI.toFixed());
+
+console.log(amount.toFixed(2));
+
+
+
+
