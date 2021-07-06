d056f71249a0760df35d189cf3e4f1ce2cf140b8
phamlethaohien <thaohienvn2007@gmail.com>
2021-07-06 08:22:03 +0700

    Lam quen voi Chuoi

---
+++ Chuỗi/Làm quen/main.js
@@ -0,0 +1,28 @@
+// Backlash (\)
+var msg = 'ABC is a \"Superhero\" ';
+
+console.log(msg);
+
+var hey = 'Đây là dấu \\';
+
+console.log(hey);
+
+// Xem độ dài chuỗi
+var fullName = 'Pham Le Thao Hien';
+
+console.log(fullName.length);
+
+// Nối chuỗi
+var match = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum, qui nesciunt? Velit odio voluptate beatae!'
++
+'Quam, laudantium, illo quae quo id soluta autem ullam incidunt fugiat sunt illum dolores esse.'
+
+console.log(match);
+
+// Template String ES6
+var firstName = 'Hien';
+var lastName = 'Pham';
+
+// console.log('I\'m ' + firstName + ' ' + lastName);
+console.log(`I\'m ${firstName} ${lastName}`);
+
