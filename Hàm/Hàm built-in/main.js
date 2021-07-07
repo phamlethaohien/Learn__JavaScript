0fac846f6c66175289351b5d65161cce238b70a0
phamlethaohien <thaohienvn2007@gmail.com>
2021-07-07 14:58:00 +0700

    All

---
+++ Hàm/Hàm built-in/main.js
@@ -0,0 +1,24 @@
+// Console
+console.log('Hello');
+
+var name = 'Thao Hien';
+console.log(name);
+
+console.warn('warning');
+console.error('error');
+
+// Confirm
+confirm('Do you want to access this website?');
+
+// Prompt
+prompt('How old are you?');
+
+// Set timeout
+setTimeout(function() {
+    alert('Tadaaa!!')
+}, 1000)
+
+// Set Interval
+setInterval(function() {
+    console.log('Hello again!')
+}, 10000)
