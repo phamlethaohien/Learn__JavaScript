77b9f94e4cee11815907dc8393056be07cee86eb
phamlethaohien <thaohienvn2007@gmail.com>
2021-07-08 07:36:09 +0700

    Cac loai function

---
+++ Hàm/Các loại function/main.js
@@ -0,0 +1,14 @@
+function showMessage() {
+    console.log('Declaration function');
+}
+
+showMessage();
+
+var showMesage2 = function() {
+    console.log('Expression function');
+}
+
+showMesage2();
+
+
+
