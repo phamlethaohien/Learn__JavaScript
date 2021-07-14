5f3b4956c06afb87d383ed4df9c513052b62edc6
phamlethaohien <thaohienvn2007@gmail.com>
2021-07-14 10:20:08 +0700

    For/of

--- Vòng lặp/For/main.js
+++ Vòng lặp/For/main.js
@@ -1,28 +1,11 @@
-var myInfo = { 
-    name: 'Thao Hien',
-    age: 14,
-    status: 'student'
-};
-
-for (var key in myInfo) {
-    console.log(myInfo[key]);
-};
-
-
-var myArray  = [
+var languages = [
     'HTML',
     'CSS',
     'JavaScript',
     'C++'
 ];
 
-for (var key2 in myArray){
-    console.log(myArray[key2]);
-}
-
-var languge = 'JavaScript';
-
-for (var key3 in languge){
-    console.log(languge[key3]);
+for (var value of languages) {
+    console.log(value);
 }
 
