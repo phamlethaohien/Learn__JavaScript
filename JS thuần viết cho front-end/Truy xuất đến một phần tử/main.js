602443f499f011a496d5ad9eab61e2c38492fbbc
phamlethaohien <thaohienvn2007@gmail.com>
2021-07-21 10:43:15 +0700

    Truy nhap den mot phan tu trong JS

---
+++ JS thuần viết cho front-end/Truy xuất đến một phần tử/main.js
@@ -0,0 +1,12 @@
+var x = document.getElementsByTagName('h1');
+console.log(x);
+
+var x1 = document.getElementsByTagName('h2');
+console.log(x1[0].innerHTML);
+
+x1[0].innerHTML = "Hello again!"
+
+var y = document.getElementsByClassName('content');
+console.log(y);
+
+
