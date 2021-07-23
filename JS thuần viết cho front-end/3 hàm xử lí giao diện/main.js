13371ba90b36089f2c6ea6c9532a305d9eaf90a9
phamlethaohien <thaohienvn2007@gmail.com>
2021-07-23 16:51:35 +0700

    3 ham xu li giao dien JS

---
+++ JS thuần viết cho front-end/3 hàm xử lí giao diện/main.js
@@ -0,0 +1,9 @@
+var x1 = document.querySelectorAll('.nut1');
+
+x1[0].classList.remove('btn-outline-primary');
+x1[0].classList.remove('btn-outline-nut1');
+x1[0].classList.add('btn-outline-warning');
+
+var x3 = document.querySelectorAll('.nut3');
+
+x3[0].classList.toggle('btn-block');
